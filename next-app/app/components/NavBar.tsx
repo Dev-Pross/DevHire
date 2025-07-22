9b11896cd6cdaa99a6e463c32635bf98b919968b
vamsi-o <iamvamsi0@gmail.com>
2025-07-22 22:45:12 +0530

    Why

--- next-app/app/components/NavBar.tsx
+++ next-app/app/components/NavBar.tsx
@@ -36,7 +36,7 @@ export  function NavBar() {
       <div className="flex flex-col items-center justify-center h-screen space-y-4">
         <h4>Welcome o this owrld </h4>
         <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-        <p className="text-lg">This is the home page of the website.</p>
+        <p className="text-lg">This is the home page the of the website.</p>
       </div>
     </div>
   );
