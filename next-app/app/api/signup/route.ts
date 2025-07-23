25fce1559d4594b8b22f0661b31dee1eadcd0690
vamsi-o <iamvamsi0@gmail.com>
2025-07-23 19:54:05 +0530

    Added Prisma

--- next-app/app/api/signup/route.ts
+++ next-app/app/api/signup/route.ts
@@ -1,14 +1,43 @@
+
+import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 export async function POST(req: Request) {
+    
+  try {
+    const prisma = new PrismaClient();
+
     const { username, email, password } = await req.json();
-    console.log(username, password);
-  
-    return new Response(JSON.stringify({
-      message: "Signup successful",
-      username,
-    }), {
-      status: 201,
-      headers: { "Content-Type": "application/json" },
+    const user = await prisma.users.create({
+      data: {
+        full_name: username,
+        email: email,
+        id: password,
+      },
     });
+    console.log(username, password);
+    console.log(user);
+
+    return new Response(
+      JSON.stringify({
+        message: "Signup successful",
+        username,
+      }),
+      {
+        status: 201,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return new Response(
+      JSON.stringify({
+        message: "Signup failed",
+        error: err instanceof Error ? err.message : "Unknown error",
+      }),
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
-  
-// export {handler as POST}
+}
