91b60b76ea2c4e17233079e6bdfbc4c4c5e90b72
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-08-15 18:56:22 +0530

    new app created

---
+++ frontend/devhire/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const page = () => {
+  return (
+    <>
+    <Navbar />
+    <h1>hai hello</h1>
+    </>
+  )
+}
+
+export default page
