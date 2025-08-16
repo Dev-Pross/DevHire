b36eb8898027216758ed40f4a11e7e80b5c99118
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-08-17 00:48:22 +0530

    login register completed

--- my-fe/app/Components/Navbar.tsx
+++ my-fe/app/Components/Navbar.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='max-w-7xl mx-auto m-0'>
+    <div className='max-w-7xl mx-auto m-0 sticky top-0 z-50'>
         <nav className='flex justify-between items-center text-white gap-4 p-4'>
             <div className=''>logo</div>
             <div className='flex gap-4'>
-                <a className='text-white hover:text-gray-300 ' href='#'><strong>Home</strong></a>
+                <a className='text-white hover:text-gray-300 font-bold' href='#'>Home</a>
                 <a className='text-white hover:text-gray-300 font-bold' href='#'>Pricing</a>
                 <a className='text-white hover:text-gray-300 font-bold' href='#'>About us</a>
             </div>
-            <div className=''>get started</div>
+            <a className='rounded-4xl pl-4 pr-4 border-2 p-1' href='#'>
+                Get Started
+            </a>
         </nav>
     </div>
   )
---
+++ my-fe/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import Login from '../Components/Login'
+
+const LoginPage = () => {
+  return (
+    <div className='min-h-screen'>
+    <Navbar/>
+    <Login/>
+    </div>
+  )
+}
+
+export default LoginPage
