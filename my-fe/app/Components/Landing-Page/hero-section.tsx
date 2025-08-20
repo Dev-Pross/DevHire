bbd11c1cddedf21a72b6d18d4a3f16d1aabca487
Vamsi_0 <iamvamsi0@gmail.com>
2025-08-20 16:40:52 +0530

    Styled a bit

--- my-fe/app/Components/Landing-Page/hero-section.tsx
+++ my-fe/app/Components/Landing-Page/hero-section.tsx
@@ -1,24 +1,66 @@
+import { useEffect } from "react";
+import { supabase } from "../../utiles/supabaseClient";
+import { useState } from "react";
+import getLoginUser from "@/app/utiles/getUserData";
+import Link from "next/link";
 export const HeroTalent = () => {
-  return (
+  const [user, setUser] = useState<{ email: string; user: string } | null>(
+    null
+  );
 
+  useEffect(() => {
+    async function fetchSession() {
+      const { data, error } = await supabase.auth.getSession();
+      console.log("session ", data);
+      if (error) {
+        console.error("Error fetching user:", error);
+      } else if (data.session?.user) {
+        console.log(
+          "User is logged in:",
+          data.session?.user.user_metadata.email,
+          " ",
+          data.session?.user.user_metadata.username
+        );
+        setUser({
+          email: data.session?.user.user_metadata.email,
+          user: data.session?.user.user_metadata.username,
+        });
+      } else {
+        console.log("No user is logged in.");
+        setUser(null);
+      }
+    }
+    fetchSession();
+  }, []);
+  return (
     <section className="h-screen w-full flex items-center px-30  ">
       <div className="flex-1 max-w-xl">
         <h1 className="text-6xl font-bold text-white  leading-tight mb-6">
-          Build Amazing Products withsome amazing Developers   <br />
-         
+          Build Amazing Products withsome amazing Developers <br />
         </h1>
-        
+
         <p className="text-xl text-stone-200 mb-8 leading-relaxed">
-          Transform your ideas into reality with our cutting-edge solutions.
+          {user ? (
+           <button className=" cursor-pointer border  bg-transparent  border-border-green-700 hover:bg-blue-600  text-white px-8 py-4 rounded-lg transition-colors">
+                Upload Resume
+              </button>
+          ) : (
+            <div className="flex space-x-4">
+              <Link href="/login">
+              <button className="cursor-pointer border hover:bg-blue-600 hover:border-gray-300 text-white-700  py-4 rounded-lg bg-gray-100 text-black transition-colors px-8">
+                Get Started
+              </button>
+              </Link>
+              <button className=" cursor-pointer border  bg-transparent  border-border-green-700 hover:bg-blue-600  text-white px-8 py-4 rounded-lg transition-colors">
+                Upload Resume
+              </button>
+            </div>
+          )}
         </p>
-        <div className="space-x-4">
-          <button className="bg-blue-600 hover:bg-blue-700 text-white px-8 py-4 rounded-lg transition-colors">
-            Get Started
-          </button>
-          <button className="border border-gray-300 text-gray-700 px-8 py-4 rounded-lg hover:bg-gray-50 transition-colors">
-            Learn More
-          </button>
-        </div>
+        {/* <div className="space-x-4">
+       
+       
+        </div> */}
       </div>
 
       <div className="flex-1 flex justify-end opacity-80">
@@ -28,7 +70,7 @@ export const HeroTalent = () => {
             alt="Product showcase"
             className="w-96 h-150 object-cover rounded-2xl shadow-2xl "
           />
-            <div className="absolute -top-4 -right-4 w-24 h-24 bg-blue-500 rounded-full opacity-20"></div>
+          <div className="absolute -top-4 -right-4 w-24 h-24 bg-blue-500 rounded-full opacity-20"></div>
         </div>
       </div>
     </section>
