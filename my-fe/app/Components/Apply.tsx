94b4b458bb6110d8a8774294240ff9dc4ea876e6
Vamsi_0 <iamvamsi0@gmail.com>
2025-08-23 09:58:54 +0530

    Prettier

--- my-fe/app/Components/Apply.tsx
+++ my-fe/app/Components/Apply.tsx
@@ -1,62 +1,59 @@
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import { Apply_Jobs } from "../utiles/agentsCall";
 
-"use client"
-import React, { useEffect, useRef, useState } from 'react'
-import { Apply_Jobs } from '../utiles/agentsCall';
+interface jobsData {
+  job_url: string;
+  job_description: string;
+}
 
-    interface jobsData{
-    "job_url":string,
-    "job_description":string
-    }
+interface ApplyProps {
+  url?: string;
+  userId?: string;
+  password?: string;
+  jobs?: jobsData[];
+}
 
-    interface ApplyProps {
-        url?: string;
-        userId?: string;
-        password?: string;
-        jobs?: jobsData[];
-    }
-
-
-
-const Apply : React.FC<ApplyProps> = () => {
-    
-    const steps = [
+const Apply: React.FC<ApplyProps> = () => {
+  const steps = [
     {
       label: "Processing jobs",
       description: "1%",
-      min:0
+      min: 0,
     },
     {
       label: "Talioring Resume",
       description: "10%",
-      min:10
+      min: 10,
     },
     {
       label: "Proccessing Resumes",
       description: "40%",
-      min:20
+      min: 20,
     },
     {
       label: "Applying Jobs",
       description: "85%",
-      min:85
+      min: 85,
     },
     {
       label: "Applied Successfully",
       description: "100%",
-      min:100
-    }
+      min: 100,
+    },
   ];
-    const intervalRef = useRef<number | null>(null);
-    const [currentStep, setCurrentStep] = useState(0);
-    const [response, setResponse] = useState<any>(null)
-    const [jobs, setJobs] = useState<{job_url: string, job_description: string}[]>([])
+  const intervalRef = useRef<number | null>(null);
+  const [currentStep, setCurrentStep] = useState(0);
+  const [response, setResponse] = useState<any>(null);
+  const [jobs, setJobs] = useState<
+    { job_url: string; job_description: string }[]
+  >([]);
 
-    
-    const url="https://uunldfxygooitgmgtcis.supabase.co/storage/v1/object/sign/user-resume/SRINIVAS_SAI_SARAN_TEJA%20(1).pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9iZjI4OTBiZS0wYmYxLTRmNTUtOTI3Mi0xZGNiNTRmNzNhYzAiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJ1c2VyLXJlc3VtZS9TUklOSVZBU19TQUlfU0FSQU5fVEVKQSAoMSkucGRmIiwiaWF0IjoxNzU0Mzc4OTgwLCJleHAiOjE3NTY5NzA5ODB9.1unaMom_BGXfxkvFB95XUMFLw7FOoVzMDBwzrJI8mOs"
-    const userId="linkedinpostgenerator@gmail.com"
-    const password="mDEccH86!zmGr:_"
-    
-    
+  const url =
+    "https://uunldfxygooitgmgtcis.supabase.co/storage/v1/object/sign/user-resume/SRINIVAS_SAI_SARAN_TEJA%20(1).pdf?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV9iZjI4OTBiZS0wYmYxLTRmNTUtOTI3Mi0xZGNiNTRmNzNhYzAiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJ1c2VyLXJlc3VtZS9TUklOSVZBU19TQUlfU0FSQU5fVEVKQSAoMSkucGRmIiwiaWF0IjoxNzU0Mzc4OTgwLCJleHAiOjE3NTY5NzA5ODB9.1unaMom_BGXfxkvFB95XUMFLw7FOoVzMDBwzrJI8mOs";
+  const userId = "linkedinpostgenerator@gmail.com";
+  const password = "mDEccH86!zmGr:_";
+
   //   const jobs=
   //       [
   //   {
@@ -81,176 +78,174 @@ const Apply : React.FC<ApplyProps> = () => {
   //   }
   // ]
 
-    
-  useEffect(()=>{
-    const job_data = localStorage.getItem("jobs")
-    if (job_data){
-      setJobs(JSON.parse(job_data))
+  useEffect(() => {
+    const job_data = localStorage.getItem("jobs");
+    if (job_data) {
+      setJobs(JSON.parse(job_data));
     }
-    console.log("jobs from ls: ",job_data);
-    
-  },[])
-  
-  console.log("jobs state",jobs);
-  
-  useEffect(()=>{
-    if (!jobs || jobs.length === 0)
-      return
-    async function apply(){
-        const {data,error} = await Apply_Jobs(jobs,url,userId,password)
-        if(data)
-          setResponse(data)
-        else
-          console.log("error from fetching jobs ",error);
+    console.log("jobs from ls: ", job_data);
+  }, []);
+
+  console.log("jobs state", jobs);
+
+  useEffect(() => {
+    if (!jobs || jobs.length === 0) return;
+    async function apply() {
+      const { data, error } = await Apply_Jobs(jobs, url, userId, password);
+      if (data) setResponse(data);
+      else console.log("error from fetching jobs ", error);
     }
 
-    apply()
+    apply();
 
-    if(userId)
-    {
-      intervalRef.current = window.setInterval(()=>{
-    fetch(`http://127.0.0.1:8000/apply/${userId}/progress`)
-    .then((res)=>res.json())
-    .then((data)=>{
-      console.log("progress: ",data.progress);
-      // setPercentage(data.progress)
-      let stepIndex=0
-      for (let i = 0; i < steps.length; i++) {
-          if (data.progress == 100){
-            stepIndex = 110
-          }
-          else if (data.progress >= steps[i].min) {
-            stepIndex = i;
-          } 
-          else {
-            break;
-          }
+    if (userId) {
+      intervalRef.current = window.setInterval(() => {
+        fetch(`http://127.0.0.1:8000/apply/${userId}/progress`)
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("progress: ", data.progress);
+            // setPercentage(data.progress)
+            let stepIndex = 0;
+            for (let i = 0; i < steps.length; i++) {
+              if (data.progress == 100) {
+                stepIndex = 110;
+              } else if (data.progress >= steps[i].min) {
+                stepIndex = i;
+              } else {
+                break;
+              }
+            }
+            setCurrentStep(stepIndex);
+            if (data.progress >= 100 && intervalRef.current) {
+              clearInterval(intervalRef.current);
+              intervalRef.current = null;
+            }
+          });
+      }, 2000);
+      return () => {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
         }
-        setCurrentStep(stepIndex)
-        if (data.progress >= 100 && intervalRef.current) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-          }
-    })
-    },2000)
-    return() => {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current);
-      }
-    };
-    }
-    else{
+      };
+    } else {
       console.log("user id not provided");
-      
     }
+  }, [jobs, url, userId, password]);
 
-  },[jobs, url, userId, password])
-
-  console.log("apply server output ",response);
-  
+  console.log("apply server output ", response);
 
   return (
     <>
-        <div className='h-screen'>
-            <div className='flex justify-center gap-0 bg-transparent p-18 py-20 shadow-lg shadow-[#052718]-500 w-full max-w-screen sticky top-15 mt-10  rounded-r-lg cursor-default'>
-                {
-                    steps.map((step,index)=>(
-                        <div className='flex flex-col item-start' key={step.label}>
-                            <div className="flex  items-center mr-1 h-fit">
-                                <div
-                                    className={`
+      <div className="h-screen">
+        <div className="flex justify-center gap-0 bg-transparent p-18 py-20 shadow-lg shadow-[#052718]-500 w-full max-w-screen sticky top-15 mt-10  rounded-r-lg cursor-default">
+          {steps.map((step, index) => (
+            <div className="flex flex-col item-start" key={step.label}>
+              <div className="flex  items-center mr-1 h-fit">
+                <div
+                  className={`
                                     w-15 h-15 rounded-full border-2 flex items-center justify-center transition-all duration-900 
                                     ${
-                                        index < currentStep
+                                      index < currentStep
                                         ? "bg-blue-600 border-blue-600"
                                         : index === currentStep
                                         ? "border-blue-500  shadow-white-500 bg-white "
                                         : "border-gray-600 bg-[#13182c] animate-pulse"
                                     }
                                     `}
-                                    >
-                                    {index < currentStep ? (
-                                    <svg width="30" height="30"><circle cx="15" cy="15" r="14" fill='#fff'/></svg>
-                                    ) : index === currentStep ? (
-                                        
-                                    // <div className="w-3 h-10 rounded-full bg-blue-400 animate-spin" />
-                                    <div className={`bg-[url('/spinner.svg')] w-15 h-15 bg-no-repeat`}></div>
-                                    ) : null}
-                                </div> 
-                                
-                            {/* Horixontal Line */}
-                            {index < steps.length - 1 && (
-                                <div
-                                className={`
+                >
+                  {index < currentStep ? (
+                    <svg width="30" height="30">
+                      <circle cx="15" cy="15" r="14" fill="#fff" />
+                    </svg>
+                  ) : index === currentStep ? (
+                    // <div className="w-3 h-10 rounded-full bg-blue-400 animate-spin" />
+                    <div
+                      className={`bg-[url('/spinner.svg')] w-15 h-15 bg-no-repeat`}
+                    ></div>
+                  ) : null}
+                </div>
+
+                {/* Horixontal Line */}
+                {index < steps.length - 1 && (
+                  <div
+                    className={`
                                     w-60 h-2 transition-all duration-900
                                     ${
-                                    index < currentStep - 1
+                                      index < currentStep - 1
                                         ? "bg-blue-600"
                                         : index === currentStep - 1
                                         ? "bg-blue-600 animate-pulse"
                                         : "bg-gray-700 animate-pulse"
                                     }
                                 `}
-                                />
-                            )}
-                        </div>
-                        {/* Texts */}
-                            <div className="">
-                                <div
-                                    className={`font-bold text-left pt-5 text-lg transition-colors duration-900
-                                    ${index === currentStep
+                  />
+                )}
+              </div>
+              {/* Texts */}
+              <div className="">
+                <div
+                  className={`font-bold text-left pt-5 text-lg transition-colors duration-900
+                                    ${
+                                      index === currentStep
                                         ? "text-blue-400"
                                         : index < currentStep
                                         ? "text-white"
                                         : "text-gray-500 animate-pulse"
                                     }`}
-                                    >
-                                    {step.label}
-                                </div>
-                            </div>
+                >
+                  {step.label}
+                </div>
+              </div>
 
-                            <div
-                                className={`text-md transition-colors duration-900
+              <div
+                className={`text-md transition-colors duration-900
                                 ${
-                                    index === currentStep
+                                  index === currentStep
                                     ? "text-blue-300"
                                     : index < currentStep
                                     ? "text-gray-300"
                                     : "text-gray-600 animate-pulse hidden"
                                 }`}
-                                >
-                                {step.description}
-                            </div>
-                        </div>
-                        
-                    ))
-                }
+              >
+                {step.description}
+              </div>
             </div>
-            {
-            (response && currentStep>100) && 
-            <>
-                <div className='flex p-16 px-32 justify-between'>
-                    <div className='flex justify-around'>
-                        <p className='text-white font-bold text-3xl '>Total: </p>
-                        <p className='text-white font-lg px-2 text-3xl'>{response.total_jobs} </p>
-                    </div>
-                    <div className='flex '>
-                        <p className='text-white font-bold text-3xl'>Success:</p>
-                        <p className='text-white font-lg px-2 text-3xl'>{response.successful_applications} </p>
-                    </div>
-                    <div className='flex '>
-                        <p className='text-white font-bold text-3xl'>Failed: </p>
-                        <p className='text-white font-lg px-2 text-3xl'>{response.failed_applications} </p>
-                    </div>
-                </div>
-                <div>
-                    <a href='https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED'><h1 className='text-white text-5xl text-center'>Track your application here</h1></a>
-                </div>
-            </>
-            }
+          ))}
         </div>
+        {response && currentStep > 100 && (
+          <>
+            <div className="flex p-16 px-32 justify-between">
+              <div className="flex justify-around">
+                <p className="text-white font-bold text-3xl ">Total: </p>
+                <p className="text-white font-lg px-2 text-3xl">
+                  {response.total_jobs}{" "}
+                </p>
+              </div>
+              <div className="flex ">
+                <p className="text-white font-bold text-3xl">Success:</p>
+                <p className="text-white font-lg px-2 text-3xl">
+                  {response.successful_applications}{" "}
+                </p>
+              </div>
+              <div className="flex ">
+                <p className="text-white font-bold text-3xl">Failed: </p>
+                <p className="text-white font-lg px-2 text-3xl">
+                  {response.failed_applications}{" "}
+                </p>
+              </div>
+            </div>
+            <div>
+              <a href="https://www.linkedin.com/my-items/saved-jobs/?cardType=APPLIED">
+                <h1 className="text-white text-5xl text-center">
+                  Track your application here
+                </h1>
+              </a>
+            </div>
+          </>
+        )}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Apply
+export default Apply;
