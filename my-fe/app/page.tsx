c968acf83de0d72073e6c4720d412195fc5b6462
Vamsi_0 <iamvamsi0@gmail.com>
2025-08-30 21:21:26 +0530

    Page .tsx changed

--- my-fe/app/page.tsx
+++ my-fe/app/page.tsx
@@ -1,37 +1,51 @@
-"use client";
-import { useEffect, useState } from "react";
+// "use client";
+// import { useEffect, useState } from "react";
 import Navbar from "./Components/Navbar";
 import { HeroTalent } from "./Components/Landing-Page/hero-section";
 import getLoginUser from "./utiles/getUserData";
 
-export default function Home() {
+export default async  function Home() {
 
   // const [user, setUser] = useState<any>();
+  const {data , error } = await getLoginUser()
+  console.log("session ",data)
+  if(error){
+    console.error("Error fetching user:",error)
+  }else if(data?.user){
+    console.log("User is logged in:",data?.user.user_metadata.email," ",data?.user.user_metadata.username)
+    // setUser(data.user.user_metadata);
+    sessionStorage.setItem("id",data?.user.user_metadata.sub)
+    sessionStorage.setItem("name",data?.user.user_metadata.username)
+    sessionStorage.setItem("email",data?.user.user_metadata.email)
 
-  useEffect(() => {
-    async function fetchSession() {
-      const { data, error } = await getLoginUser();
-      console.log("session ", data);
-      if (error) {
-        console.error("Error fetching user:", error);
-      } else if (data?.user) {
-        console.log(
-          "User is logged in:",
-          data?.user.user_metadata.email,
-          " ",
-          data?.user.user_metadata.username
-        );
-        // setUser(data.user.user_metadata);
-        sessionStorage.setItem("id",data?.user.user_metadata.sub)
-        sessionStorage.setItem("name",data?.user.user_metadata.username)
-        sessionStorage.setItem("email",data?.user.user_metadata.email)
+  }else{
+    console.log("No user is logged in.")
+  }
 
-      } else {
-        console.log("No user is logged in.");
-      }
-    }
-    fetchSession();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchSession() {
+  //     const { data, error } = await getLoginUser();
+  //     console.log("session ", data);
+  //     if (error) {
+  //       console.error("Error fetching user:", error);
+  //     } else if (data?.user) {
+  //       console.log(
+  //         "User is logged in:",
+  //         data?.user.user_metadata.email,
+  //         " ",
+  //         data?.user.user_metadata.username
+  //       );
+  //       // setUser(data.user.user_metadata);
+  //       sessionStorage.setItem("id",data?.user.user_metadata.sub)
+  //       sessionStorage.setItem("name",data?.user.user_metadata.username)
+  //       sessionStorage.setItem("email",data?.user.user_metadata.email)
+
+  //     } else {
+  //       console.log("No user is logged in.");
+  //     }
+  //   }
+  //   fetchSession();
+  // }, []);
 
   return (
     <div
