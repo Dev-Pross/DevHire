d0c2de1434935976f7bfc08faa5e7e0ee00b526a
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-10-31 13:12:44 +0530

    Update installation scripts to include aiohttp and ensure pip is upgraded consistently

--- backend_python/run.bat
+++ backend_python/run.bat
@@ -1,3 +1,5 @@
 pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install aiohttp
 python -m playwright install chromium
--- backend_python/run.sh
+++ backend_python/run.sh
@@ -1,5 +1,7 @@
 pip install --upgrade pip
+python.exe -m pip install --upgrade pip
 pip install -r requirements.txt
+pip install aiohttp
 apt install tesseract-ocr poppler-utils
 apt-get install tesseract-ocr poppler-utils
 playwright install chromium
