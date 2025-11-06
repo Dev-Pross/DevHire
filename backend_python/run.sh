2c0c2ef69d7f072e594c54c6276886dfb3b9b0df
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-11-06 18:47:44 +0530

    removed tessract installation

--- backend_python/run.sh
+++ backend_python/run.sh
@@ -1,6 +1,4 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 pip install aiohttp
-apt install tesseract-ocr poppler-utils
-apt-get install tesseract-ocr poppler-utils
 playwright install chromium
