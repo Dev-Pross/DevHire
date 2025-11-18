7453a0828ec37a835832612ffd2858ab152e5cbc
TejaBudumuru3 <tejabudumuru3@gmail.com>
2025-11-18 20:45:21 +0530

    Refactor Jobs component: improve code formatting, enhance readability, and streamline state management

--- my-fe/app/Components/Jobs.tsx
+++ my-fe/app/Components/Jobs.tsx
@@ -1,61 +1,64 @@
-"use client"
-import React, { useEffect, useRef, useState } from 'react';
-import JobCards from './JobCards';
-import {sendUrl} from '../utiles/agentsCall';
-import CryptoJS from 'crypto-js';
-import toast from 'react-hot-toast';
-import { useRouter } from 'next/navigation';
-import { API_URL } from '../utiles/api';
-import getLoginUser from '../utiles/getUserData';
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import JobCards from "./JobCards";
+import { sendUrl } from "../utiles/agentsCall";
+import CryptoJS from "crypto-js";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { API_URL } from "../utiles/api";
+import getLoginUser from "../utiles/getUserData";
 
-  const steps = [
-    {
-      label: "Process started",
-      description: "Fetching relevant Job Titles",
-      min:0
-    },
-    {
-      label: "Job Titles found!",
-      description: "10%",
-      min:10
-    },
-    {
-      label: "Getting Jobs from server",
-      description: "40%",
-      min:20
-    },
-    {
-      label: "Processing Jobs",
-      description: "85%",
-      min:85
-    },
-    {
-      label: "Select the Jobs to apply!",
-      description: "100%",
-      min:100
-    }
-  ];
+const steps = [
+  {
+    label: "Process started",
+    description: "Fetching relevant Job Titles",
+    min: 0,
+  },
+  {
+    label: "Job Titles found!",
+    description: "10%",
+    min: 10,
+  },
+  {
+    label: "Getting Jobs from server",
+    description: "40%",
+    min: 20,
+  },
+  {
+    label: "Processing Jobs",
+    description: "85%",
+    min: 85,
+  },
+  {
+    label: "Select the Jobs to apply!",
+    description: "100%",
+    min: 100,
+  },
+];
 
 const Jobs = () => {
-
   const intervalRef = useRef<number | null>(null);
   const [currentStep, setCurrentStep] = useState(0);
   const [jobs, setJobs] = useState<any>();
   const [url, setUrl] = useState<string>("");
-  const [userId, setUserId]  =useState<string>("")
-  const [password, setPassword] = useState<string>("")
-  const [Progress_userId, setProgress_UserId] = useState<string>("")
-  const [linkedin_context, setLinkedin_context] = useState<any>()
+  const [userId, setUserId] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [Progress_userId, setProgress_UserId] = useState<string>("");
+  const [linkedin_context, setLinkedin_context] = useState<any>();
   const [callAgent, setCallAgent] = useState<boolean>(false);
 
-  const router = useRouter()
+  const router = useRouter();
 
   // let jwt=""
 
-  const ENC_KEY = "qwertyuioplkjhgfdsazxcvbnm987456" // ensure 32 bytes for AES-256
-  const IV = "741852963qwerty0"
+  const ENC_KEY = "qwertyuioplkjhgfdsazxcvbnm987456"; // ensure 32 bytes for AES-256
+  const IV = "741852963qwerty0";
 
-  function decryptData(ciphertextBase64: string, keyStr: string, ivStr: string) {
+  function decryptData(
+    ciphertextBase64: string,
+    keyStr: string,
+    ivStr: string
+  ) {
     const key = CryptoJS.enc.Utf8.parse(keyStr);
     const iv = CryptoJS.enc.Utf8.parse(ivStr);
 
@@ -64,36 +67,33 @@ const Jobs = () => {
       mode: CryptoJS.mode.CBC,
       padding: CryptoJS.pad.Pkcs7,
     });
-  return decrypted.toString(CryptoJS.enc.Utf8);
-}
-  useEffect(()=>{
+    return decrypted.toString(CryptoJS.enc.Utf8);
+  }
+  useEffect(() => {
     async function user() {
       const email = sessionStorage.getItem("email");
-     
-      if (email){
-        console.log(email)
+
+      if (email) {
+        console.log(email);
         setProgress_UserId(email);
-      }else{
+      } else {
         console.log("no email found");
-        
       }
     }
-    user()
-  },[])
+    user();
+  }, []);
 
-   useEffect(() => {
-    
-    const pdf = sessionStorage.getItem("resume")
-    const context = sessionStorage.getItem("Lcontext")
-    if(pdf)
-      setUrl(pdf)
-    else{
-      toast.error("Resume not found. Please upload your resume")
-      router.push("/Jobs/LinkedinUserDetails")
+  useEffect(() => {
+    const pdf = sessionStorage.getItem("resume");
+    const context = sessionStorage.getItem("Lcontext");
+    if (pdf) setUrl(pdf);
+    else {
+      toast.error("Resume not found. Please upload your resume");
+      router.push("/Jobs/LinkedinUserDetails");
     }
     async function fetchEncryptedCredentials() {
       try {
-  const res = await fetch("/api/get-data", {
+        const res = await fetch("/api/get-data", {
           method: "GET",
           headers: { "Content-Type": "application/json" },
           credentials: "include",
@@ -107,104 +107,89 @@ const Jobs = () => {
           const credentials = JSON.parse(decrypted);
           setUserId(credentials.username);
           setPassword(credentials.password);
-          setCallAgent(true)
+          setCallAgent(true);
           // console.log("Decrypted user credentials:", userId," ",password);
         } else {
           // console.error("No encryptedData in response");
-          toast.error("Linkedin credentials not provided")
-          router.push("/Jobs/LinkedinUserDetails")
+          toast.error("Linkedin credentials not provided");
+          router.push("/Jobs/LinkedinUserDetails");
         }
-      } catch (error:any) {
+      } catch (error: any) {
         // console.error("Error fetching or decrypting credentials:", error);
-        toast.error("Error caused by: ",error.message)
+        toast.error("Error caused by: ", error.message);
       }
     }
 
-    if(context != "true"){
+    if (context != "true") {
       console.log("no context present in session storage");
       fetchEncryptedCredentials();
-    }
-    else{
+    } else {
       console.log("context present in session storage");
-      
     }
-    setLinkedin_context(context)
+    setLinkedin_context(context);
   }, []);
 
   // console.log("encrypted data: ", jwt)
-  
-  useEffect(()=>{
-    const fetchJobs = async()=>{
-      if(callAgent){
-      if(url==="" || linkedin_context ===""){
-        console.log("all parameters are empty")
-        console.log("url: ", url)
-        console.log("LC: ", linkedin_context)
-        return
-      }
-      else{
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      if (url === "" || linkedin_context === "") {
+        console.log("all parameters are empty");
+        console.log("url: ", url);
+        console.log("LC: ", linkedin_context);
+        return;
+      } else {
         // console.log("calling agent to fetch jobs");
-        const {data ,error} = await sendUrl(url,userId,password)
-        if(data)
-          setJobs(data.jobs)
-        else
-          toast.error(`Error from fetching jobs ${error}`)
+        if (linkedin_context === "true" || callAgent) {
+          const { data, error } = await sendUrl(url, userId, password);
+          if (data) setJobs(data.jobs);
+          else toast.error(`Error from fetching jobs ${error}`);
           // console.log("error from fetching jobs ",error);
-
-      }
-    }
-    }
-
-    fetchJobs()
-    if(Progress_userId)
-    {
-      intervalRef.current = window.setInterval(()=>{
-  fetch(`http://localhost:8000/jobs/${Progress_userId}/progress`)
-    .then((res)=>res.json())
-    .then((data)=>{
-      // console.log("progress: ",data.progress);
-      // setPercentage(data.progress)
-      let stepIndex=0
-      for (let i = 0; i < steps.length; i++) {
-          if (data.progress == 100){
-            stepIndex = 110
-          }
-          else if (data.progress >= steps[i].min) {
-            stepIndex = i;
-          } 
-          else {
-            break;
-          }
         }
-        setCurrentStep(stepIndex)
-        if (data.progress >= 100 && intervalRef.current) {
-            clearInterval(intervalRef.current);
-            intervalRef.current = null;
-          }
-    })
-    },10000)
-    return() => {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current);
       }
     };
-    }
-    else{
+
+    fetchJobs();
+    if (Progress_userId) {
+      intervalRef.current = window.setInterval(() => {
+        fetch(`http://localhost:8000/jobs/${Progress_userId}/progress`)
+          .then((res) => res.json())
+          .then((data) => {
+            // console.log("progress: ",data.progress);
+            // setPercentage(data.progress)
+            let stepIndex = 0;
+            for (let i = 0; i < steps.length; i++) {
+              if (data.progress == 100) {
+                stepIndex = 110;
+              } else if (data.progress >= steps[i].min) {
+                stepIndex = i;
+              } else {
+                break;
+              }
+            }
+            setCurrentStep(stepIndex);
+            if (data.progress >= 100 && intervalRef.current) {
+              clearInterval(intervalRef.current);
+              intervalRef.current = null;
+            }
+          });
+      }, 10000);
+      return () => {
+        if (intervalRef.current) {
+          clearInterval(intervalRef.current);
+        }
+      };
+    } else {
       console.log("user id not provided");
-      
     }
+  }, [url, linkedin_context, Progress_userId, callAgent, userId, password]);
 
-  },[url,linkedin_context, Progress_userId, callAgent, userId, password ])
-
- 
-
-// console.log("current step: ",currentStep); 
-// console.log("jwt:",jwt);
- 
+  // console.log("current step: ",currentStep);
+  // console.log("jwt:",jwt);
 
   return (
-    <div className='flex flex-col lg:flex-row jobs-section min-h-screen'>
-      <div className='flex flex-col gap-0 bg-gradient-to-b from-[#244283] to-[#0e3661] p-4 md:p-6 lg:p-8 py-8 lg:py-20 shadow-lg shadow-[#052718]-500 w-full lg:w-auto lg:max-w-xs lg:sticky lg:top-15 lg:left-0 lg:h-screen rounded-b-lg lg:rounded-r-lg lg:rounded-b-none cursor-default'>
+    <div className="flex flex-col lg:flex-row jobs-section min-h-screen">
+      <div className="flex flex-col gap-0 bg-gradient-to-b from-[#244283] to-[#0e3661] p-4 md:p-6 lg:p-8 py-8 lg:py-20 shadow-lg shadow-[#052718]-500 w-full lg:w-auto lg:max-w-xs lg:sticky lg:top-15 lg:left-0 lg:h-screen rounded-b-lg lg:rounded-r-lg lg:rounded-b-none cursor-default">
         {steps.map((step, index) => (
           <div className="flex items-start" key={step.label}>
             {/* Connector Line & Circle Col */}
@@ -223,7 +208,12 @@ const Jobs = () => {
                 `}
               >
                 {index < currentStep ? (
-                  <svg className="w-2.5 h-2.5 md:w-3 md:h-3" viewBox="0 0 11 11"><circle cx="5.5" cy="5.5" r="5" fill="#fff" /></svg>
+                  <svg
+                    className="w-2.5 h-2.5 md:w-3 md:h-3"
+                    viewBox="0 0 11 11"
+                  >
+                    <circle cx="5.5" cy="5.5" r="5" fill="#fff" />
+                  </svg>
                 ) : index === currentStep ? (
                   <div className="w-3 h-3 md:w-3.5 md:h-3.5 rounded-full bg-[#1ab5a9] animate-spin" />
                 ) : null}
@@ -248,11 +238,12 @@ const Jobs = () => {
             <div className="pb-2 md:pb-2">
               <div
                 className={`font-bold text-sm md:text-base transition-colors duration-900
-                  ${index === currentStep
-                    ? "text-blue-400"
-                    : index < currentStep
-                    ? "text-white"
-                    : "text-gray-500 animate-pulse"
+                  ${
+                    index === currentStep
+                      ? "text-blue-400"
+                      : index < currentStep
+                      ? "text-white"
+                      : "text-gray-500 animate-pulse"
                   }`}
               >
                 {step.label}
@@ -273,10 +264,10 @@ const Jobs = () => {
           </div>
         ))}
       </div>
-      <div className='w-full h-auto p-4 md:p-6 lg:p-8'>
-        {currentStep>100 ?
-          (<JobCards jobs={jobs}/>):
-          (
+      <div className="w-full h-auto p-4 md:p-6 lg:p-8">
+        {currentStep > 100 ? (
+          <JobCards jobs={jobs} />
+        ) : (
           <span className="flex items-center justify-center min-h-[60vh] lg:min-h-[80vh] text-white gap-2 flex-col">
             <svg
               className="animate-spin h-8 w-8 md:h-10 md:w-10 text-white"
@@ -299,8 +290,7 @@ const Jobs = () => {
             </svg>
             <p className="text-sm md:text-base">loading jobs... </p>
           </span>
-          )
-        }
+        )}
       </div>
     </div>
   );
