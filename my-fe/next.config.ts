0df9bb5a1f6c0da3afae2600d32139369ca3ff2f
BUDUMURU SRINIVAS SAI SARAN TEJA <iamtejabudumuru@gmail.com>
2026-01-14 20:32:21 +0530

    feat: add API rewrites for dynamic routing to AWS endpoint

--- my-fe/next.config.ts
+++ my-fe/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*', // The URL your frontend calls (e.g. /api/users)
+        destination: 'http://13.232.102.49:8000/:path*', // Your AWS HTTP IP (e.g. http://54.12.3.4/users)
+      },
+    ]
+  },
 };
 
 export default nextConfig;
